ad2552eb525e8f910a72a8d670185f4c4086854d
tonsojun <josephjun@gmail.com>
2021-04-04 14:19:43 -0700

    added foreign key along with select drop down menu

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  resources :authors
+  resources :authors 
   resources :books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: "books#index"
+
+  get '/authors', to: 'authors#index'
+
 end
